9978d0c6b7264ecd0ec82f3603b5398e47909cad
Rizki Kurnia <rizkidiego614@gmail.com>
2025-11-05 22:43:57 +0700

    commit dari laptop lain (masih dalam lingkup backend)

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -1,2 +1,6 @@
-// Database URL for development
-export const DATABASE_URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8/sikes";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+export const DATABASE_URL = process.env.DATABASE_URL || "";
